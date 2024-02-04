937cd5d1ade16cfab73efca0198ad32eccd5bf89
Madiha Muhammad <madiha.muhammad.dev@gmail.com>
2024-02-05 02:22:50 +0500

    destructure in fun body

--- basics/src/components/Greet.js
+++ basics/src/components/Greet.js
@@ -6,7 +6,23 @@ import React from "react";
 - destructuring > unpack values from arrays or props from objs
 - 2 ways : in props
 */
-const Greet = ({ name, task, children }) => {
+// const Greet = ({ name, task, children }) => {
+//   console.log("name: ", name, ", task: ", task, ", children: ", children);
+//   // console.log(props);
+//   return (
+//     <h1>
+//       {/* destructure */}
+//       Hi Greetings. This is {name} {task}
+//       {children}
+//       {/* Hi Greetings. This is {props.name} {props.task}
+//       {props.children} */}
+//     </h1>
+//   );
+// };
+
+// Destructure in fun body
+const Greet = (props) => {
+  const { name, task, children } = props;
   console.log("name: ", name, ", task: ", task, ", children: ", children);
   // console.log(props);
   return (
