fdd91c4bc8362c0928766c23643bbb4904e6a25c
Madiha Muhammad <madiha.muhammad.dev@gmail.com>
2024-02-08 14:20:38 +0500

    take out the map() mean logic, to make code simpler

--- basics/src/components/NameList.js
+++ basics/src/components/NameList.js
@@ -2,11 +2,17 @@ import React from "react";
 
 function NameList() {
   const names = ["Bruce", "Clark", "Diana"];
+  const nameList = names.map((name) => <h2>{name}</h2>);
+
   return (
     <div>
-      {names.map((name) => (
-        <h2>{name}</h2>
-      ))}
+      {
+        nameList
+        // move out logic to make code simple
+        // names.map((name) => (
+        //     <h2>{name}</h2>
+        // ))
+      }
 
       {/* use map method to render a list */}
       {/* <h2>{names[0]}</h2>
