9d2d56a47d63e9e2b61448c6d39f9a68796c8ee8
Madiha Muhammad <madiha.muhammad.dev@gmail.com>
2024-02-08 12:12:02 +0500

    conditions: element var approach

--- basics/src/components/UserGreeting.js
+++ basics/src/components/UserGreeting.js
@@ -5,14 +5,32 @@ class UserGreeting extends Component {
     super(props);
 
     this.state = {
-      isLoggedin: true,
+      isLoggedin: false,
     };
   }
 
   render() {
-    // if else can't work in jsx, jsx is only for fun calls & obj construction
-    if (this.state.isLoggedin) return <div>Hi Welcome!</div>;
-    else return <div>Welcome Greetings!</div>;
+    /*
+    1st approach:
+     if else can't work in jsx, 
+     jsx is only for fun calls & obj construction
+     */
+    // if (this.state.isLoggedin) return <div>Hi Welcome!</div>;
+    // else return <div>Welcome Greetings!</div>;
+
+    /*
+    2nd approach: better approach
+    usig element var, js var use to store elements
+    can store whole componenet or part of it
+     */
+    let message;
+    if (this.state.isLoggedin) {
+      message = <div>element in var if section weoking</div>;
+    } else {
+      message = <div>element in var else section weoking</div>;
+    }
+
+    return <div>{message}</div>;
 
     // return (
     //   <div>
