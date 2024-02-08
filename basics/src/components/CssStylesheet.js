efde30adfdd155d5fb7f34e2d815eae30b72de39
Madiha Muhammad <madiha.muhammad.dev@gmail.com>
2024-02-08 20:14:28 +0500

    template literals use while need  to apply multiple classes for styling

--- basics/src/components/CssStylesheet.js
+++ basics/src/components/CssStylesheet.js
@@ -7,6 +7,10 @@ function CssStylesheet(props) {
   return (
     <div>
       <h1 className={className}>CSS Style Sheet</h1>
+      <h2 className={`${className} font-xl`}>
+        Specify multiple styling classes, use template literals. enclose
+        class-names in backtics, if prop use $ as prefix{" "}
+      </h2>
     </div>
   );
 }
