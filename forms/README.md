0bbd145f34d2e1a0867259bfb7a4613e86d63199
Madiha Muhammad <madiha.muhammad.dev@gmail.com>
2024-02-09 16:28:06 +0500

    controlled componenet: for Input element

--- forms/README.md
+++ forms/README.md
@@ -1,1 +1,7 @@
 # ReactJs
+
+#### Forms
+
+- in regular htmlform elements like input, textarea etc handle input and respective value by theirself directly
+- Controll Components: react haldled their input and values
+- 
---
+++ forms/src/components/Form.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+
+class Form extends Component {
+  /* make input as controlled element:
+    1- in ctor initilize the state prop will null > prop_name: ''
+     - assign state with value of input-element > value={this.state.prop_name}
+    2- handle onCHange event
+     - add onChange event with handle in input-elem > onChange={this.handle_name} 
+     - now define it a slass peop, and use prop
+  */
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      //  1st step
+      username: "",
+    };
+  }
+
+  //   2nd step
+  handleUsernameChange = (event) => {
+    this.setState({
+      username: event.target.value,
+    });
+    console.log(this.state.username);
+  };
+
+  render() {
+    return (
+      <form>
+        <div>
+          <label>User Name: </label>
+          {/* 1st assign state with value */}
+          {/* handle onChange event */}
+          <input
+            type="text"
+            value={this.state.username}
+            onChange={this.handleUsernameChange}
+          />
+        </div>
+      </form>
+    );
+  }
+}
+
+export default Form;
