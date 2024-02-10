15fa6959bed76238dfaf091015ebb4a3e03b911f
Madiha Muhammad <madiha.muhammad.dev@gmail.com>
2024-02-10 17:19:58 +0500

    Fragments: can accept key attributes of list items

--- advance/src/components/Columns.js
+++ advance/src/components/Columns.js
@@ -1,6 +1,9 @@
 import React from "react";
 
 function Columns() {
+  // array of items
+  const items = [];
+
   return (
     /*
     Console Warning: Warning: validateDOMNesting(...): <td> cannot appear as a child of <div>.
@@ -10,6 +13,15 @@ function Columns() {
 
     // <div>
     <React.Fragment>
+      {/* can accept "key" attribute when rendering list of items
+            Key attributes are only one acceptable by react fragment
+       */}
+      {items.map((item) => (
+        <React.Fragment key={item.id}>
+          <h1>Title</h1>
+          <p>{item.description}</p>
+        </React.Fragment>
+      ))}
       <td>Name</td>
       <td>Madiha</td>
       {/* </div> */}
